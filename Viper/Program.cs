2fc27c8c03fd90443e9f4e36218d691839e1d0dc
Ryan Sporcic <ryan.sporcic@snhu.edu>
2024-02-27 13:02:53 -0500

    Test commit. Added a button to Study form.

--- Viper/Program.cs
+++ Viper/Program.cs
@@ -16,7 +16,7 @@ namespace Viper
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new frmStudy());
         }
     }
 }
--- Viper/Study.cs
+++ Viper/Study.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Viper
 {
-    public partial class Form1 : Form
+    public partial class frmStudy : Form
     {
-        public Form1()
+        public frmStudy()
         {
             InitializeComponent();
         }
